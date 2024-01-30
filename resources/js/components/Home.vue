8b9b2ce77580879db0761f91d27e45ebdd4e0122
Kisley.Viana <48730446+kisley-viana@users.noreply.github.com>
2024-01-30 09:34:33 -0300

    Instalação Vue
    Projeto com o Vue instalado

---
+++ resources/js/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Welcome to a LaravelVue Project!</h1>
+        <h2>{{ devName }}</h2>
+    </div>
+</template>
+<script>
+    export default{
+        setup:() => ({
+            devName:'Kisley Viana'
+        }),
+    }
+</script>
+<style scoped>
+/* CSS Here! */
+</style>
